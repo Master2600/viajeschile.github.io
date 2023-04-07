35bf495f5fd1b4e12c4ea98d055b65a3030a1545
Juan Escudero <Jotaebe@gmail.com>
2023-04-07 19:14:30 -0400

    corregido espacio en blanco entre footer y contacto, se documenta archivo css,

--- assets/js/mijs.js
+++ assets/js/mijs.js
@@ -1,13 +1,14 @@
 $ (document).ready (function(){
-
+/* funcion click aenviar correo*/
     $("#enviarCorreo").click(function(){
       alert("El correo fue enviado correctamente")
     })
 
+/*captura click menu para ir lentamente a secciones de la pagina*/
     $('a.scroll').click(function(event) {
         event.preventDefault();
         var pincha = $(this).attr("href");
-        $('html, body, nav').animate({
+        $('html,nav').animate({
           scrollTop: $(pincha).offset().top
         }, 1500); 
       });
